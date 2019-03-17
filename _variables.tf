73efa68a1d395c06071af952f8be0a71fd0aa79c
Allan Denot <adenot@gmail.com>
2019-03-17 18:18:32 +1100

    Initial commit: module to create one aws account

---
+++ _variables.tf
@@ -0,0 +1,7 @@
+variable "name" {
+  description = "Name for the organization and prefix for all accounts"
+}
+
+variable "email" {
+  description = "Email address for this account, needs to be unique"
+}
---
+++ account.tf
@@ -0,0 +1,5 @@
+resource "aws_organizations_account" "account" {
+  name                       = "${var.name}"
+  email                      = "${var.email}"
+  iam_user_access_to_billing = "DENY"
+}
