8b401caa96609a687f11bc9ab60ffc5b27ff6992
Allan Denot <adenot@gmail.com>
2020-03-11 13:59:32 +1100

    Outputting email for use in the stack

--- _outputs.tf
+++ _outputs.tf
@@ -2,3 +2,8 @@ output "account_id" {
   value       = aws_organizations_account.account.id
   description = "ID of the account created"
 }
+
+output "email" {
+  value       = var.email
+  description = "Email of account"
+}
