1cbb4f0923067f00571ebdd46e42c6a21ef0ab19
Allan Denot <adenot@gmail.com>
2020-05-07 14:49:17 +1000

    Ignoring iam_user_access_to_billing as it might not exist on imported accounts

--- account.tf
+++ account.tf
@@ -2,4 +2,8 @@ resource "aws_organizations_account" "account" {
   name                       = var.name
   email                      = var.email
   iam_user_access_to_billing = "DENY"
+
+  lifecycle {
+    ignore_changes = [iam_user_access_to_billing]
+  }
 }
