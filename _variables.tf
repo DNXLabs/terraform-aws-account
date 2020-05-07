a7f8f88e5e8312f71169ba772204b4878bfe2b29
Allan Denot <adenot@gmail.com>
2020-05-07 15:00:59 +1000

    Adding optional parameter for when account name in the organization has spaces or other characters

--- _variables.tf
+++ _variables.tf
@@ -2,6 +2,11 @@ variable "name" {
   description = "Name for the account to be created"
 }
 
+variable "ssm_name" {
+  default     = ""
+  description = "Name of account to use on SSM path. Only required if name has invalid characters"
+}
+
 variable "email" {
   description = "Email address for this account, needs to be unique"
 }
